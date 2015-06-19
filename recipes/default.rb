3cc3f850b37e06da11bd690f351f4c32957bd99e
Juan Nunez <nunezj2@gmail.com>
2015-06-19 11:33:13 -0400

    Update default.rb. Pass variable to erb. Update motd.erb read databag items.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,14 @@
+nagiosData = search(:dsw_nagios, "id:default")
+nagiosData.at(0)["profiles"].each {|x|
+	puts x["host"]
+	puts x["profile"]
+}
+
+
 template "motd" do
   path "#{node[:motd][:dir]}/motd"
   source "motd.erb"
+  variables :test => nagiosData.at(0)["profiles"]
   #owner "root"
   #group "root"
   #mode "0644"
